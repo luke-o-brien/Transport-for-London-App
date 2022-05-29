a31a2f80702b3cb20ae69972676d59b035450390
Luke O'Brien <lukeobrien02@gmail.com>
2022-05-29 21:18:14 +0100

    Service severity description added + Live departures search + routing

--- src/components/Elements/StationSearch.js
+++ src/components/Elements/StationSearch.js
@@ -1,29 +1,45 @@
 import react from "react";
+import { Link } from "react-router-dom";
 import styles from "./StationSearch.module.css"
 
 function StationSearch() {
 
   const [stationdata, setStationData] = react.useState(undefined)
+  const [name, setName] = react.useState("")
+  //const [title, setTitle] = useState('')
+  
+  
+  
 
-  react.useEffect(()=> {
-    async function getStationId() {
-      const response = await fetch("https://api.tfl.gov.uk/StopPoint/Search?query=new")
-      const data = await response.json()
-      setStationData(data.matches)
-      console.log(stationdata)
 
-    }
-    getStationId()
-  }, []);
+  async function getStationId(e) {
+    const _name = e.target.value
+    setName(_name);
+    console.log(_name)
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/Search?query=${_name}`)
+    const data = await response.json()
+    setStationData(data.matches)
+    console.log(stationdata)
 
-  return ( 
-    stationdata ? stationdata.map((station) => {
-      return <div key={station.id}>
-        {station.modes.includes("tube") || station.modes.includes("overground") || station.modes.includes("dlr") ? 
-          <div className={styles.name_container}>
-            <h3 className={styles.name}>{station.name}</h3> <p>{station.modes}</p></div> : null}
-      </div>
-    }) : <p>loading data</p>
+  
+  }
+  return ( <>
+    <div>
+      <input className={styles.inputField} onChange={ getStationId }  value={name} placeholder="Enter Station eg. Oxford Circus" />
+    </div>  
+    <div className={styles.resultsContainer}>
+      {stationdata ? stationdata.map((station) => {
+        return <div key={station.id}>
+          <Link to={`/Live-Departures/${station.name}`}>
+            {station.modes.includes("tube") || station.modes.includes("overground") || station.modes.includes("dlr") || station.modes.includes("elizabeth-line") ? 
+              <div className={styles.name_container}>
+                <h3 className={styles.name}>{station.name}</h3>
+              </div> : null}
+          </Link>
+        </div>
+      }) : <p className={styles.startTyping}>Start Typing to display Stations</p>}
+    </div>
+  </>
   )
 
 }
--- src/components/Elements/StatusUpdate/Lines.js
+++ src/components/Elements/StatusUpdate/Lines.js
@@ -3,6 +3,11 @@ import styles from "./Lines.module.css"
 
 function Lines() {
   const [lineStatus, setLineStatus] = React.useState(undefined) 
+  const [isShown, setIsShown] = React.useState(false)
+
+  function handleClick() {
+    setIsShown(!isShown)
+  }
   
   React.useEffect(() => {
     async function GetServiceData() {
@@ -21,8 +26,13 @@ function Lines() {
       return <div className={styles[line.id]} key={line.id}>
         <h3 className={styles.line_name}>{line.name}</h3>
         {line.lineStatuses.map((status) => {
-          return <div key={line.name}>
-            <p className={styles.line_status}>{status.statusSeverityDescription}</p> 
+          return <div className={styles.statusSeverityContainer} key={line.name}>
+            {/* if Line is running with good service [status 10] or is shut for night [status 20] display no more info*/}
+            {/* if Line is running with problems [any other status code] display caution icon and clickablemore details a element */}
+            {status.statusSeverity === 10 || status.statusSeverity === 20 ? 
+              <p className={styles.line_status}>{status.statusSeverityDescription}</p> : <p className={styles.line_status}>{status.statusSeverityDescription}<i onClick={handleClick} className="fa-solid fa-circle-info"></i></p>}
+            {/* show reason for problem unless the line is closed [status 20] as the information is given in the h3 element */}
+            {(isShown && status.statusSeverity !== 20) && <p>{status.reason}</p>}
           </div>
         })}
       </div> 
@@ -31,3 +41,5 @@ function Lines() {
 }
 
 export default Lines
+
+//<i class="fa-solid fa-circle-info"></i>
