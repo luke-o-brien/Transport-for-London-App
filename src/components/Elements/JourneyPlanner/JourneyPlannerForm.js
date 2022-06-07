6ffb11de4877fc9f315279af3179038c5fd8a6c8
Luke O'Brien <lukeobrien02@gmail.com>
2022-06-07 21:35:53 +0100

    autocomplete station search as origin form submit function written

---
+++ src/components/Elements/JourneyPlanner/JourneyPlannerForm.js
@@ -0,0 +1,65 @@
+import react from "react"
+import { createRoutesFromChildren } from "react-router-dom"
+import styles from "./JourneyPlannerForm.module.css"
+
+function JourneyPlannerForm() {
+
+  
+  const [stationdata, setStationData] = react.useState(undefined)
+  const [name, setName] = react.useState("")
+  const [orgin, setOrigin] = react.useState("")
+
+  async function getStationId(e) {
+    const _name = e.target.value
+    setName(_name);
+    console.log(_name)
+    const response = await fetch(`https://api.tfl.gov.uk/StopPoint/Search?query=${_name}`)
+    const data = await response.json()
+    setStationData(data.matches)
+    console.log(stationdata)
+  }
+
+  function handleclick(e) {
+    e.preventDefault();
+    console.log("clicked")
+    const selected = e.target.value
+    const text = e.target.innerHTML
+    setOrigin(selected)
+    console.log(selected)
+    const field = document.getElementById("start") 
+    field.value = text
+    setName(text)
+    
+
+  }
+
+  function submit(e) {
+    e.preventDefault();
+    console.log("submited");
+    //const start = 
+    console.log(`the submitted origin is ${orgin}`)
+  }
+
+  return (<>
+    <p>form</p>
+    <form className={styles.form}>
+      <label>
+        <input id="start" className={styles.TextInput} onChange={ getStationId } value={name} placeholder="eltham"></input>
+      </label>
+      <div className={styles.stationSuggestions}>
+        {stationdata ? stationdata.slice(0, 5).map((station) => {
+          return <div key={station.id}>
+            {station.modes.includes("tube") || station.modes.includes("dlr") || station.modes.includes("elizabeth-line") || station.modes.includes("national-rail") ? 
+              <div className={styles.suggestionContainer}>
+                <button value={station.icsId} onClick={ handleclick } className={styles.Suggestion}>{(station.name).replace("Underground Station", "")}</button>
+              </div> : null}
+          </div>
+        }) : null}  
+      </div>
+      <input onClick={submit} type="submit"></input>
+    </form>
+  </>
+  )
+}
+
+export default JourneyPlannerForm
---
+++ src/components/Pages/JourneyPlanner.js
@@ -0,0 +1,29 @@
+import react from "react";
+import { useState } from "react/cjs/react.production.min";
+import JourneyPlannerForm from "../Elements/JourneyPlanner/JourneyPlannerForm";
+
+function JourneyPlanner() {
+
+  // //const [journeyData, setJourneyData] = react.useState(undefined)
+  
+  // react.useEffect(() => {
+  //   async function getJourneyData() {
+  //     const response = await fetch("https://api.tfl.gov.uk/Journey/JourneyResults/910GELTHAM/to/910GFELTHAM")
+  //     const data = await response.json()
+  //     const _journeyData = data
+  //    // setJourneyData(_journeyData)
+  //     console.log(_journeyData)
+  //   }
+  //   getJourneyData()
+  // }, []);
+
+
+
+  return ( <>
+    <p>journey planner page</p>
+    <JourneyPlannerForm />
+  </>
+  )
+}
+
+export default JourneyPlanner
