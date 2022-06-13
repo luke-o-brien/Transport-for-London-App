109b9adaade84728565b7e708b7ab9bf69edd301
Luke O'Brien <lukeobrien02@gmail.com>
2022-06-13 20:30:12 +0100

    working toggle div jp

--- src/components/Elements/JourneyPlanner/JourneyPlannerResults.js
+++ src/components/Elements/JourneyPlanner/JourneyPlannerResults.js
@@ -7,11 +7,17 @@ function JourneyResults(props) {
   const origin = props["origin"]
   const dest = props["dest"]
   const time = props["time"].replace(":", "")
-  console.log(time)
 
   const [journeyData, setJourneyData] = react.useState(undefined)
   const [showStopPoints, setShowStopPoints] = react.useState(false)
   const [showRouteDetail, setShowRouteDetail] = react.useState(false)
+  const [activeIndex, setActiveIndex] = react.useState(null)
+
+  function showRouteDetails(index) {
+    console.log(index)
+    setActiveIndex(index)
+    setShowRouteDetail(!showRouteDetail)
+  }
   
   react.useEffect(() => {
     async function getJourneyData() {
@@ -28,23 +34,21 @@ function JourneyResults(props) {
     setShowStopPoints(!showStopPoints)
   }
 
-  function showRouteDetails() {
-    setShowRouteDetail(!showRouteDetail)
-  }
+  
 
   return (journeyData ? <>
-    {journeyData.journeys.slice(0, 3).map((journey, pos) => {
-      return <div key={pos} className={styles.ResultsDiv}>
+    {journeyData.journeys.slice(0, 5).map((journey, index) => {
+      return <div key={index} className={styles.ResultsDiv}>
         <div className={styles.ResultsDivContent}>
           <div className={styles.JourneyTimes}>
             <p>{journey.startDateTime.split("T")[1].split(":").slice(0,-1).join(":")} - {journey.arrivalDateTime.split("T")[1].split(":").slice(0,-1).join(":")}</p>
-            {(journey.duration > 60) ? <p>{Math.floor(journey.duration / 60)} hr {journey.duration % 60} Mins</p> : <p>( {journey.duration} min )</p>}
+            {(journey.duration > 60) ? <p>{Math.floor(journey.duration / 60)} hr {journey.duration % 60} mins</p> : <p> {journey.duration} min </p>}
           </div>
-          <a className={styles.routeDetailsButton} onClick={showRouteDetails} >View Route Details<i className="fa-solid fa-angle-down"></i></a>
+          <a className={styles.routeDetailsButton} onClick={() => showRouteDetails(index)}>View Route Details<i className="fa-solid fa-angle-down"></i></a>
         </div>
         
-        { showRouteDetail && journey.legs.map((legs, pos) => {
-          return <div key={pos} className={`${styles.back} ${legs.mode.name === "walking" || legs.mode.name === "bus" || legs.mode.name === "coach" ? styles[legs.mode.name] : styles[legs.routeOptions[0].name.replace(/\s/g,"-")]}`}>
+        {journey.legs.map((legs, pos) => {
+          return (activeIndex === index) && showRouteDetail ? <div key={pos} className={`${styles.back} ${legs.mode.name === "walking" || legs.mode.name === "bus" || legs.mode.name === "coach" ? styles[legs.mode.name] : styles[legs.routeOptions[0].name.replace(/\s/g,"-")]}`}>
             <div className={styles.legDetailsContainer}>
               {/* <p className={styles.details}>{legs.mode.name.replace("-", " " )}</p> */}
               <p className={styles.detailsDepArr}>{legs.departurePoint.commonName}</p> 
@@ -61,7 +65,7 @@ function JourneyResults(props) {
               <p className={styles.detailsDepArr}>{legs.arrivalPoint.commonName}</p>
               <p className={styles.details}>{(legs.arrivalTime.split("T")[1].split(":").slice(0,-1).join(":"))}</p>
             </div>
-          </div>
+          </div> : null
         })}
       
       </div>
